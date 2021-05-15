b6afd49fff58054b15c165fb7115ebc520ed8946
KhrystynaVasko <khristinavasko152001@gmail.com>
2021-05-15 10:02:45 +0300

    Migrated files

---
+++ credentials_sample.php
@@ -0,0 +1,17 @@
+<?php
+
+$cred = array(
+    'dev' => array(
+        'db_name' => '',
+        'db_user' => '',
+        'db_password' => '',
+        'db_host' => '',
+
+    ),
+    'prod' => array(
+        'db_name' => '',
+        'db_user' => '',
+        'db_password' => '',
+        'db_host' => '',
+    ),
+);
